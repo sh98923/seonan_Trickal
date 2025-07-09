6a08517a0dacb959ae1a382607ac83c1c244ff3c
seonan <sh98923@naver.com>
2025-07-09 21:11:35 +0900

    .
    .

---
+++ Assets/Scripts/System/BattleStart.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BattleStart : MonoBehaviour
+{
+    [SerializeField] private GameObject _deckPanel;
+    [SerializeField] private GameObject _selectCardPanel;
+    [SerializeField] private GameObject _rerollImage;
+    [SerializeField] private GameObject _startBtn;
+
+    private bool isDeckMode = false;
+
+    public void OnClickStart()
+    {
+        _deckPanel.SetActive(false);
+        _startBtn.SetActive(false);
+
+        _selectCardPanel.SetActive(true);
+        _rerollImage.SetActive(true);
+
+        isDeckMode = true;
+    }
+
+    private void Update()
+    {
+        if (isDeckMode && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _selectCardPanel.SetActive(false);
+            _rerollImage.SetActive(false);
+
+            _deckPanel.SetActive(true);
+            _startBtn.SetActive(true);
+
+            isDeckMode = false;
+        }
+    }
+}
